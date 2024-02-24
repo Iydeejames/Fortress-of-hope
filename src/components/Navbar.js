a7bb41bcccc3e87d66fc08a09d79c11cb22531a1
Idaraobong James <iydeejaymes@gmail.com>
2024-02-24 16:10:57 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
 import react, {useState, useEffect} from 'react';
 
 const Navbar = () => {
-    
-}
+    const[MenuOpen, setMenuOpen] = useState(false);
+    const[scrolled, setScrolled] = useState(false);
+
+    useEffect( () => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 50);
+        };
+    })
+
+};
 
 export default Navbar;
