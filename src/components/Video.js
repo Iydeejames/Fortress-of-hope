7cc413252dcbd61265fe8c5d7bd6951d2491a9d0
Idaraobong James <iydeejaymes@gmail.com>
2024-02-27 14:07:00 +0100

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,12 +6,21 @@ import video1 from '../assets/imgs/FI8.MOV';
 import video2 from '../assets/imgs/FI9.MOV';
 
 const VideoSection = () => {
-  const [isPlaying, setPlaying] = useState(false);
+  const [isPlaying, setPlaying] = useState(Array(6).fill(false));
+  const videoRefs = Array(6).fill(null).map(() => useRef(null));
 
   const videoUrls = [video1, video1, video1, video2, video2, video2];
 
-  const handleTogglePlay = () => {
-    setPlaying(!isPlaying);
+  const handleTogglePlay = (index) => {
+    const updatedPlaying = [...isPlaying];
+    updatedPlaying[index] = !updatedPlaying[index];
+    setPlaying(updatedPlaying);
+
+    if (updatedPlaying[index]) {
+      videoRefs[index].current.play();
+    } else {
+      videoRefs[index].current.pause();
+    }
   };
 
   return (
@@ -21,15 +30,16 @@ const VideoSection = () => {
         {[...Array(6)].map((_, index) => (
           <div key={index} className="video-card">
             <video
+              ref={videoRefs[index]}
               src={videoUrls[index]}
               controls={false}
               autoPlay={false}
               loop={true}
               muted={true}
-              className={isPlaying ? 'playing' : ''}
+              className={isPlaying[index] ? 'playing' : ''}
             />
-            <div className="play-button" onClick={handleTogglePlay}>
-              <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+            <div className="play-button" onClick={() => handleTogglePlay(index)}>
+              <FontAwesomeIcon icon={isPlaying[index] ? faPause : faPlay} />
             </div>
           </div>
         ))}
