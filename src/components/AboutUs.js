3a631517eb1bed6262688213a1b39780614e2346
Idaraobong James <iydeejaymes@gmail.com>
2024-02-27 20:20:00 +0100

    Update AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const AboutSection = () => {
+  // Function to handle click on the smaller cards
+  const handleCardClick = (cardId) => {
+    // Logic to open another page or perform any other action
+    console.log(`Card ${cardId} clicked`);
+  };
+
+  return (
+    <div className="about-section-container">
+      <div className="about-section-left">
+        <img src="/path/to/your/image.jpg" alt="About" />
+      </div>
+      <div className="about-section-right">
+        <div className="main-card">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla...</p>
+        </div>
+        <div className="small-cards" onClick={() => handleCardClick(1)}>
+          <img src="/path/to/your/image1.jpg" alt="Card 1" />
+        </div>
+        <div className="small-cards" onClick={() => handleCardClick(2)}>
+          <img src="/path/to/your/image2.jpg" alt="Card 2" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
