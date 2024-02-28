a427f765de0ee5df16614b7679d9c8d0d1e7890c
Idaraobong James <iydeejaymes@gmail.com>
2024-02-28 10:28:59 +0100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,17 +9,17 @@ const AboutSection = () => {
       <div className="about-section-container">
         <div className="about-section-left">
           <h2>About Us</h2>
-          <img src={process.env.PUBLIC_URL + '/assets/imgs/FI2.JPG'} alt="About" />
+          <img src={process.env.PUBLIC_URL + '/assets/imgs/FI2.PNG'} alt="About" />
         </div>
         <div className="about-section-right">
           <div className="main-card">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla...</p>
           </div>
           <div className="small-cards">
-            <img src="/src/assets/imgs/FI3.JPG" alt="Card 1" />
+            <img src={process.env.PUBLIC_URL + '/assets/imgs/FI3.JPG'} alt="Card 1" />
           </div>
           <div className="small-cards">
-            <img src="/src/assets/imgs/FI5.JPG" alt="Card 2" />
+            <img src={process.env.PUBLIC_URL + '/assets/imgs/FI5.JPG'} alt="Card 2" />
           </div>
         </div>
       </div>
