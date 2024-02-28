a6a4f37ae705c04131a300dd288c1cb324056f72
Idaraobong James <iydeejaymes@gmail.com>
2024-02-28 12:47:22 +0100

    Multiple additions and deletions

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,23 +8,32 @@ import FI5 from "../assets/imgs/FI5.JPG";
 const AboutSection = () => {
     return (
       <div className="about-section-container">
-                  <h2>About Us</h2>
-        <div className="about-section-left">
+        <div className="about-section-heading">
+        <h2>Rev. Prince Amba</h2>
+        </div>
 
+          <div className="about-section-left">
           <img src={FI2} alt="About" />
         </div>
         <div className="about-section-right">
           <div className="main-card">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla...</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla...
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla </p>
           </div>
-          <div className="small-cards">
-            <img src={FI3} alt="Card 1" />
-          </div>
-          <div className="small-cards">
-            <img src={FI5} alt="Card 2" />
-          </div>
+
         </div>
-      </div>
+        </div>
+
     );
   };
   
