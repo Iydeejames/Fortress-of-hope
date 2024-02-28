9b3addda259c0c4c61e61010c39b6e8551d52726
Idaraobong James <iydeejaymes@gmail.com>
2024-02-28 10:29:59 +0100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,22 +4,23 @@ import FI3 from "../assets/imgs/FI3.JPG";
 import FI5 from "../assets/imgs/FI5.JPG";
 
 
+
 const AboutSection = () => {
     return (
       <div className="about-section-container">
         <div className="about-section-left">
           <h2>About Us</h2>
-          <img src={process.env.PUBLIC_URL + '/assets/imgs/FI2.PNG'} alt="About" />
+          <img src={FI2} alt="About" />
         </div>
         <div className="about-section-right">
           <div className="main-card">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla...</p>
           </div>
           <div className="small-cards">
-            <img src={process.env.PUBLIC_URL + '/assets/imgs/FI3.JPG'} alt="Card 1" />
+            <img src={FI3} alt="Card 1" />
           </div>
           <div className="small-cards">
-            <img src={process.env.PUBLIC_URL + '/assets/imgs/FI5.JPG'} alt="Card 2" />
+            <img src={FI5} alt="Card 2" />
           </div>
         </div>
       </div>
