8ddfdd1bf3f19d5c34ed7ecce14188009073a8dc
Idaraobong James <iydeejaymes@gmail.com>
2024-02-28 13:51:51 +0100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,9 +5,10 @@ import FI2 from "../assets/imgs/FI2.PNG";
 const AboutSection = () => {
     return (
       <div className="about-section-container">
-
-          <div className="about-section-left">
-          <img src={FI2} alt="About" />
+        <div className="about-section-left">
+          <div className="image-card">
+            <img src={FI2} alt="About" />
+          </div>
         </div>
         <div className="about-section-right">
           <div className="main-card">
