58f2f1ab47a4e95f22898eeda8b5851b437e80d4
Idaraobong James <iydeejaymes@gmail.com>
2024-02-29 22:00:26 +0100

    rdits

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import React, { useEffect, useState } from 'react';
 import FI2 from "../assets/imgs/FI2.PNG";
 
 
 const AboutSection = () => {
-    const [backgroundColor, setBackgroundColor] = useState("rgba(0, 0, 0, 0.1)");
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const opacity = Math.min(scrollPosition / 500, 1); // Adjust the divisor for the scroll sensitivity
-        setBackgroundColor(`rgba(0, 0, 0, ${opacity})`);
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
     return (
-      <div className="about-section-container" style={{ backgroundColor }}>
+      <div className="about-section-container">
         <div className="about-section-left">
           <div className="image-card">
             <img src={FI2} alt="About" />
