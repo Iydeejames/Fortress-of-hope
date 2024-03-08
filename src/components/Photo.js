7b0b8386d89ff724fd7211c88ada8d4d0d863ace
Idaraobong James <iydeejaymes@gmail.com>
2024-03-07 22:58:21 -0800

    edits

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,10 +6,9 @@ import image3 from '../assets/imgs/FI12.jpg';
 import image4 from '../assets/imgs/FI16.jpg';
 import image5 from '../assets/imgs/FI11.jpg';
 import image6 from '../assets/imgs/FI10.jpg';
-import image7 from '../assets/imgs/FI13.jpg';
 import image8 from '../assets/imgs/FI11.jpg'
 
-const images = [image1, image2, image3, image4, image5, image6, image7, image8];
+const images = [image1, image2, image3, image4, image5, image6, image8];
 
 const PhotoSection = () => {
     return (
