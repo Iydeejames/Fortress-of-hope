d268e7b05a391cea82b20d29ac92fb3e42974a03
Idaraobong James <iydeejaymes@gmail.com>
2024-03-07 23:19:06 -0800

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -13,7 +13,6 @@ const images = [image1, image2, image3, image4, image5, image6, image8];
 const PhotoSection = () => {
     return (
       <div className="photo-section-container">
-        <div className="wavy-background"></div>
         <div className="photo-section">
           {images.map((image, index) => (
             <img key={index} src={image} alt={`image-${index + 1}`} />
