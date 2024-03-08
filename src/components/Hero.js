a444e6088f9389e88451d3fc68f8669f6b4efcf9
Idaraobong James <iydeejaymes@gmail.com>
2024-03-07 22:25:27 -0800

    More Additions

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import FI11 from "../assets/imgs/FI11.jpg";
+import FI16 from "../assets/imgs/FI16.jpg";
 //import FI3 from "../assets/imgs/FI3.JPG";
 //import FI4 from "../assets/imgs/FI4.JPG";
 //import FI5 from "../assets/imgs/FI5.JPG";
 
 const HeroSection = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const images = [FI11];
+    const images = [FI16];
     //const images = [FI3, FI4, FI5];
   
     useEffect(() => {
