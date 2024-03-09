9e6f62e0ae614f8138b6c7d450720a1468b3f785
Idaraobong James <iydeejaymes@gmail.com>
2024-03-08 21:24:56 -0800

    edits

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import image1 from '../assets/imgs/FI3.JPG';
-import image2 from '../assets/imgs/FI12.jpg';
-import image3 from '../assets/imgs/FI16.jpg';
-import image4 from '../assets/imgs/FI11.jpg';
-import image5 from '../assets/imgs/FI4.JPG';
+import image2 from '../assets/imgs/FI4.JPG';
+import image3 from '../assets/imgs/FI12.jpg';
+import image4 from '../assets/imgs/FI16.jpg';
+import image5 from '../assets/imgs/FI11.jpg';
 import image6 from '../assets/imgs/FI10.jpg';
 import image8 from '../assets/imgs/FI11.jpg';
 
