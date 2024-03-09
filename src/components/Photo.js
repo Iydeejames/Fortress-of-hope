40092f3b1e8b8d2caf8ce896e2c674cce3a9e424
Idaraobong James <iydeejaymes@gmail.com>
2024-03-08 21:14:40 -0800

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,22 +6,25 @@ import image3 from '../assets/imgs/FI12.jpg';
 import image4 from '../assets/imgs/FI16.jpg';
 import image5 from '../assets/imgs/FI11.jpg';
 import image6 from '../assets/imgs/FI10.jpg';
-import image8 from '../assets/imgs/FI11.jpg'
+import image8 from '../assets/imgs/FI11.jpg';
 
 const images = [image1, image2, image3, image4, image5, image6, image8];
 
 const PhotoSection = () => {
-    return (
-      <div className="photo-section-container">
-        <div className='floating-bg'>
+  // Duplicate the images to create a seamless loop
+  const duplicatedImages = [...images, ...images];
+
+  return (
+    <div className="photo-section-container">
+      <div className='floating-bg'>
         <div className="photo-section">
-          {images.map((image, index) => (
+          {duplicatedImages.map((image, index) => (
             <img key={index} src={image} alt={`image-${index + 1}`} />
           ))}
-          </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default PhotoSection;
