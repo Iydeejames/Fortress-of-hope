ce05287b1c49dd1773301a9616b5b2e829e38abd
Idaraobong James <iydeejaymes@gmail.com>
2024-03-09 21:39:27 -0800

    smh

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import image1 from '../assets/imgs/FI3.JPG';
 import image2 from '../assets/imgs/FI12.jpg';
 import image3 from '../assets/imgs/FI16.jpg';
@@ -11,18 +10,44 @@ import image8 from '../assets/imgs/FI11.jpg';
 const images = [image1, image2, image3, image4, image5, image6, image8];
 
 const PhotoSection = () => {
-    return (
-      <div className="photo-section-container">
-        <div className='floating-bg'>
+  const imageWidthPercentage = 100 / images.length;
+
+  return (
+    <div className="photo-section-container">
+      <div className='floating-bg'>
         <div className="photo-section">
           {images.map((image, index) => (
-            <img key={index} src={image} alt={`image-${index + 1}`} />
+            <img key={index} src={image} alt={`image-${index + 1}`} style={{ width: `${imageWidthPercentage}%` }} />
           ))}
-          </div>
         </div>
       </div>
-    );
-  }; 
+      <style>
+        {`
+          .photo-section {
+            display: flex;
+            animation: slideAnimation ${images.length * 2}s linear infinite;
+          }
+
+          @keyframes slideAnimation {
+            0% {
+              transform: translateX(0%);
+            }
+            100% {
+              transform: translateX(-${100}%);
+            }
+          }
+
+          .photo-section img {
+            margin-top: 4rem;
+            margin-bottom: 4rem;
+            max-width: 100%;
+            height: 20vh;
+            margin-right: -1px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default PhotoSection;
-
