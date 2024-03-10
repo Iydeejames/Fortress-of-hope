66b6a61f3dc6a6bf9317b36e3243bd5626a1ef7d
Idaraobong James <iydeejaymes@gmail.com>
2024-03-09 21:28:41 -0800

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
 import image1 from '../assets/imgs/FI3.JPG';
-import image2 from '../assets/imgs/FI4.JPG';
-import image3 from '../assets/imgs/FI12.jpg';
-import image4 from '../assets/imgs/FI16.jpg';
-import image5 from '../assets/imgs/FI11.jpg';
+import image2 from '../assets/imgs/FI12.jpg';
+import image3 from '../assets/imgs/FI16.jpg';
+import image4 from '../assets/imgs/FI11.jpg';
+import image5 from '../assets/imgs/FI4.JPG';
 import image6 from '../assets/imgs/FI10.jpg';
 import image8 from '../assets/imgs/FI11.jpg';
 
 const images = [image1, image2, image3, image4, image5, image6, image8];
 
 const PhotoSection = () => {
-  return (
-    <div className="photo-section-container">
-      <div className="floating-bg">
+    return (
+      <div className="photo-section-container">
+        <div className='floating-bg'>
         <div className="photo-section">
           {images.map((image, index) => (
-            <div key={index} className="image-container">
-              <img src={image} alt={`image-${index + 1}`} />
-            </div>
+            <img key={index} src={image} alt={`image-${index + 1}`} />
           ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }; 
 
 export default PhotoSection;
+
