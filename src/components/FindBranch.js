c705b1a9a8139ae68cdca2dc78f229e62066bfeb
Idaraobong James <iydeejaymes@gmail.com>
2024-03-19 23:37:19 -0700

    Update FindBranch.js

--- src/components/FindBranch.js
+++ src/components/FindBranch.js
@@ -1,6 +1,12 @@
 import React from 'react'; 
 
 const FindBranch = () => {
-    
-}
-export default FindBranch;
+    return (
+      <div className="find-branch-container">
+        <h2 className="find-branch-text">Find a Branch closest to you</h2>
+        <button className="find-branch-button">Branches</button>
+      </div>
+    );
+  };
+  
+  export default FindBranch;
