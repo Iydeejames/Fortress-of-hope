e965d5a14dbfb5d564c23b33f3e755a560aba54c
Idaraobong James <iydeejaymes@gmail.com>
2024-03-21 20:56:09 -0700

    Additions

--- src/components/Salvation.js
+++ src/components/Salvation.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 const SalvationSection = () => {
   const [showSalvationPage, setShowSalvationPage] = useState(false);
 
@@ -17,14 +16,14 @@ const SalvationSection = () => {
       {!showSalvationPage ? (
         <div>
           <h2>Ready to begin this new experience with Christ?</h2>
-          <button onClick={handleYesClick}>YES</button>
+          <button onClick={handleYesClick} className="yes-button">YES</button>
         </div>
       ) : (
         <div className="salvation-page">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
-          <button onClick={handleBackClick}>Back</button>
+          <button onClick={handleBackClick} className="back-button">Back</button>
         </div>
       )}
     </div>
