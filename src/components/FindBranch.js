6a3c637918d8c9d62c596ae6bcde0ee8fb922c92
Idaraobong James <iydeejaymes@gmail.com>
2024-03-23 20:12:56 -0700

    edits

--- src/components/FindBranch.js
+++ src/components/FindBranch.js
@@ -8,12 +8,10 @@ const FindBranch = () => {
 
     const handleBranchClick = () => {
         setShowBranchDetails(true);
-        document.querySelector('.main-content').style.opacity = '0'; // Setting opacity of main content to 0
     };
 
     const handleCloseDetails = () => {
         setShowBranchDetails(false);
-        document.querySelector('.main-content').style.opacity = '1'; // Restoring opacity of main content to 1
     };
 
     return (
@@ -39,7 +37,7 @@ const FindBranch = () => {
                                     Fortress Of Hope Gospel Church, Nwaniba road, opposite NDLEA, Uyo.<br></br>
                                     <b>Our time of services are:</b> <br></br>
                                     Sunday: 7am and 9am<br></br>
-                                    wednesday: 5pm <br></br>
+                                    Wednesday: 5pm <br></br>
                                 </p>
                             </div>
                             <div className="branch-item">
@@ -49,7 +47,7 @@ const FindBranch = () => {
                                     Fortress Of Hope Gospel Church, Uyo.<br></br>
                                     <b>Our time of services are:</b> <br></br>
                                     Sunday: 7am and 9am<br></br>
-                                    wednesday: 5pm <br></br>
+                                    Wednesday: 5pm <br></br>
                                 </p>
                             </div>
                             <div className="branch-item">
@@ -59,7 +57,7 @@ const FindBranch = () => {
                                     Fortress Of Hope Gospel Church, Uyo.<br></br>
                                     <b>Our time of services are:</b> <br></br>
                                     Sunday: 7am and 9am<br></br>
-                                    wednesday: 5pm <br></br>
+                                    Wednesday: 5pm <br></br>
                                 </p>
                             </div>
                         </div>
