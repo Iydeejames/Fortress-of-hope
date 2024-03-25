b0ae9e0f178f39e5298f506d77d38d9445ffe183
Idaraobong James <iydeejaymes@gmail.com>
2024-03-25 14:29:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
+// App.js
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import HeroSection from './components/Hero';
 import BannerSection from './components/Banner';
 import VideoSection from './components/Video';
 import AboutSection from './components/AboutUs';
-import PhotoSection from './components/Photo'
-import FindBranch from './components/FindBranch'
-import SalvationSection from './components/Salvation'
-import EventSection from './components/Events'
+import PhotoSection from './components/Photo';
+import FindBranch from './components/FindBranch';
+import SalvationSection from './components/Salvation';
+import EventSection from './components/Events';
+import BranchDetails from './components/BranchDetails'; // Import BranchDetails component
 
 function App() {
   return (
-    <div className="App">
-      < Navbar/>
-      < HeroSection/>
-      < AboutSection/>
-      < VideoSection/>
-      < SalvationSection />
-      < PhotoSection/>
-      < FindBranch/>
-      < BannerSection/>
-      < EventSection/>
-    </div>
-
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={HeroSection} />
+          <Route path="/about" component={AboutSection} />
+          <Route path="/video" component={VideoSection} />
+          <Route path="/salvation" component={SalvationSection} />
+          <Route path="/photo" component={PhotoSection} />
+          <Route path="/find-branch" component={FindBranch} />
+          <Route path="/banner" component={BannerSection} />
+          <Route path="/events" component={EventSection} />
+          <Route path="/branch-details" component={BranchDetails} /> {/* Add this route */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
