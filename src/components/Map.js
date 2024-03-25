8f9078f1d40ecdd43b9c467b9ac14305a685894c
Idaraobong James <iydeejaymes@gmail.com>
2024-03-24 22:08:23 -0700

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,64 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const ChicagoMap = () => {
-  let map;
-
-  useEffect(() => {
-    const chicago = { lat: 41.85, lng: -87.65 };
-
-    const createCenterControl = () => {
-      const controlButton = document.createElement("button");
-      // Set CSS for the control.
-      controlButton.classList.add('buttonStyle');
-
-      controlButton.textContent = "Center Map";
-      controlButton.title = "Click to recenter the map";
-      controlButton.type = "button";
-      // Setup the click event listeners: simply set the map to Chicago.
-      controlButton.addEventListener("click", () => {
-        map.setCenter(chicago);
-      });
-      return controlButton;
-    }
-
-    const initMap = () => {
-      map = new window.google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: { lat: 49.496675, lng: -102.65625 },
-      });
-
-      var georssLayer = new window.google.maps.KmlLayer({
-        url: "http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss",
-      });
-      georssLayer.setMap(map);
-
-      // Create the DIV to hold the control.
-      const centerControlDiv = document.createElement("div");
-      // Create the control.
-      const centerControl = createCenterControl(map);
-
-      // Append the control to the DIV.
-      centerControlDiv.appendChild(centerControl);
-      map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(
-        centerControlDiv
-      );
-    }
-
-    if (!window.google) {
-      // If Google Maps API not loaded, do nothing or handle accordingly
-      console.error("Google Maps API not loaded!");
-      return;
-    }
-
-    initMap();
-
-  }, []);
-
+const Map = () => {
   return (
-    <div style={{ height: "100vh" }}>
-      <div id="map" style={{ height: "100%" }} />
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
-export default ChicagoMap;
+export default Map;
