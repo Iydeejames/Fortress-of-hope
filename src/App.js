dc669f6b305bd544b9ab99652e220651d8993c24
Idaraobong James <iydeejaymes@gmail.com>
2024-03-26 16:26:30 -0700

    App bug fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 import HeroSection from './components/Hero';
 import BannerSection from './components/Banner';
 import VideoSection from './components/Video';
@@ -9,28 +9,21 @@ import PhotoSection from './components/Photo';
 import FindBranch from './components/FindBranch';
 import SalvationSection from './components/Salvation';
 import EventSection from './components/Events';
-import BranchDetails from './components/BranchDetails';
 
 function App() {
   return (
     <div className="App">
-      < Navbar/>
-      < HeroSection/>
-      < AboutSection/>
-      < VideoSection/>
-      < SalvationSection />
-      < PhotoSection/>
-      < FindBranch/>
-      < BannerSection/>
-      < EventSection/>
-      < BranchDetails/>
-
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <VideoSection />
+      <SalvationSection />
+      <PhotoSection />
+      <FindBranch />
+      <BannerSection />
+      <EventSection />
     </div>
-
   );
 }
 
 export default App;
-
-
-
