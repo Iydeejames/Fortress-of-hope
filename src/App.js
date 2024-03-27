2aa3592d4761f418fc08680d01dcab1b923c916c
Idaraobong James <iydeejaymes@gmail.com>
2024-03-26 23:35:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/Hero';
@@ -7,10 +7,18 @@ import VideoSection from './components/Video';
 import AboutSection from './components/AboutUs';
 import PhotoSection from './components/Photo';
 import FindBranch from './components/FindBranch';
+import BranchDetails from './components/BranchDetails'; // Import BranchDetails component
 import SalvationSection from './components/Salvation';
 import EventSection from './components/Events';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('FindBranch'); // Add state for current page
+
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Render different components based on the current page state
   return (
     <div className="App">
       <Navbar />
@@ -19,12 +27,12 @@ function App() {
       <VideoSection />
       <SalvationSection />
       <PhotoSection />
-      <FindBranch />
+      {currentPage === 'FindBranch' && <FindBranch navigateTo={navigateTo} />}
+      {currentPage === 'BranchDetails' && <BranchDetails navigateTo={navigateTo} />}
       <BannerSection />
       <EventSection />
     </div>
   );
 }
-
 
 export default App;
