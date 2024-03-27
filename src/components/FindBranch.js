0443a4f4e77e4c1d6049f2c0c52c0f05b452e582
Idaraobong James <iydeejaymes@gmail.com>
2024-03-26 23:28:19 -0700

    60% of bugs fixed

--- src/components/FindBranch.js
+++ src/components/FindBranch.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import BranchDetails from './BranchDetails';
-
-const FindBranch = () => {
-  const [showBranchDetails, setShowBranchDetails] = useState(false);
-
-  const handleShowBranchDetails = () => {
-    setShowBranchDetails(true);
-  };
-
+const FindBranch = ({ handleShowBranchDetails }) => {
   return (
     <div className="find-branch-container">
-      {!showBranchDetails && (
-        <>
-          <h2>Find a Branch nearest to you</h2>
-          <button onClick={handleShowBranchDetails}>Locations</button>
-        </>
-      )}
-      {showBranchDetails && <BranchDetails />}
+      <h2>Find a Branch nearest to you</h2>
+      <button onClick={handleShowBranchDetails}>Locations</button>
     </div>
   );
 };
