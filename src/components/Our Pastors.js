85988d63b5da4d1f964208706c7e29ed4dd95062
Idaraobong James <iydeejaymes@gmail.com>
2024-04-01 00:27:33 -0700

    Multiple Additions

--- src/components/Our Pastors.js
+++ src/components/Our Pastors.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import firstPhoto from '../assets/imgs/FI3.JPG';
-import SecondPhoto from '../assets/imgs/FI3.JPG';
+import firstPhoto from '../assets/imgs/FI5.JPG';
+import SecondPhoto from '../assets/imgs/FI21.JPG';
 
 const PastorsSection = ()=> {
 
 
     return (
         <div className='pastors-section'>
-            <h2> </h2>
+            <h2> Our Pastors</h2>
+            <div className='cards-container'>
+                <div className='card-content'>
+                The mission of this organization is to be independent of any political ideology, religious doctrine, and economic interest but rather a value-adding organization driving the
+                 African development process through every positive means.
+                 To ensure the active participation of youths in the development of Africa.
+                </div>
+            </div>
         </div>
     ) 
 }
+
+export default PastorsSection;
