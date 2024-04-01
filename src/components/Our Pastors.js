27fd187cf0f379c4142e353785732a3cf5121280
Idaraobong James <iydeejaymes@gmail.com>
2024-03-31 22:40:01 -0700

    Update Our Pastors.js

---
+++ src/components/Our Pastors.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import firstPhoto from '../assets/imgs/FI3.JPG';
+import SecondPhoto from '../assets/imgs/FI3.JPG';
+
+const PastorsSection = ()=> {
+
+
+    return (
+        <div className='pastors-section'>
+            <h2> </h2>
+        </div>
+    ) 
+}
