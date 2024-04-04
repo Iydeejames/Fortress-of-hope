081ac6ea25bdc2ee64594c57a1d3322ae131f32f
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-04 16:52:15 -0700

    Update OurPastors.js

--- src/components/OurPastors.js
+++ src/components/OurPastors.js
@@ -1,50 +1,49 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import firstPhoto from '../assets/imgs/FI5.JPG';
 import secondPhoto from '../assets/imgs/FI21.JPG';
+import './PastorsSection.css'; // Import CSS file for styling
 
-const PastorsSection = ()=> {
+const PastorsSection = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleStory = () => {
+        setIsOpen(!isOpen);
+    };
 
- return (
- <div className='pastors-section'>
-    <h2>Meet Our Pastors</h2>
-    <div className='cards-container'>
-        <div className='card-content'>
-          <img src={firstPhoto} alt='Rev Amba'/>
-          <h3> Rev. Princa Amba
-          <span> General Overseer</span>
-          </h3>
-           <div className='story-btn'>About
-           <div className='story-btn-line'></div>
-           </div>
-           <div class="story">
-                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur, 
-                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
-                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora
-                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
-                        </p>
+    return (
+        <div className='pastors-section'>
+            <h2>Meet Our Pastors</h2>
+            <div className='cards-container'>
+                <div className='card-content'>
+                    <img src={firstPhoto} alt='Rev Amba' />
+                    <h3>Rev. Princa Amba<span>General Overseer</span></h3>
+                    <div className='story-btn' onClick={toggleStory}>
+                        About
+                        <div className='story-btn-line'></div>
                     </div>
-    </div>
+                    {isOpen && (
+                        <div className='story'>
+                            <p className='story-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet pariatur ullam tempore tempora.</p>
+                        </div>
+                    )}
+                </div>
 
-    <div className='card-content'>
-          <img src={secondPhoto} alt='Dr. Evelyn'/>
-          <h3> Dr. Evelyn Amba
-          <span> Resident Pastor</span>
-          </h3>
-           <div className='story-btn'>About
-           <div className='story-btn-line'></div>
-           </div>
-           <div class="story">
-                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur,
-                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
-                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora
-                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.</p>
+                <div className='card-content'>
+                    <img src={secondPhoto} alt='Dr. Evelyn' />
+                    <h3>Dr. Evelyn Amba<span>Resident Pastor</span></h3>
+                    <div className='story-btn' onClick={toggleStory}>
+                        About
+                        <div className='story-btn-line'></div>
                     </div>
-    </div>
-    </div>
- </div>
-  ) 
-}
+                    {isOpen && (
+                        <div className='story'>
+                            <p className='story-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet pariatur ullam tempore tempora.</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default PastorsSection;
