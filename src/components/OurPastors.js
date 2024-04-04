c9815159b39246c335fc71a820f7bf3114c11518
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-04 16:24:42 -0700

    Update OurPastors.js

--- src/components/OurPastors.js
+++ src/components/OurPastors.js
@@ -15,33 +15,30 @@ const PastorsSection = ()=> {
           <h3> Rev. Princa Amba
           <span> General Overseer</span>
           </h3>
-           <p>The mission of this organization is to be independent of any political ideology,
-            religious doctrine, and economic interest but rather a value-adding organization driving the
-            African development process through every positive means.
-            To ensure the active participation of youths in the development of Africa.
-           </p>
            <div className='story-btn'>About
            <div className='story-btn-line'></div>
            </div>
            <div class="story">
-                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.</p>
+                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
+                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora
+                        adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
+                        </p>
                     </div>
     </div>
 
     <div className='card-content'>
           <img src={secondPhoto} alt='Dr. Evelyn'/>
           <h3> Dr. Evelyn Amba</h3>
-           <p>The mission of this organization is to be independent of any political ideology,
-            religious doctrine, and economic interest but rather a value-adding organization driving the
-            African development process through every positive means.
-            To ensure the active participation of youths in the development of Africa.
-           </p>
            <div className='story-btn'>About
            <div className='story-btn-line'></div>
            </div>
            <div class="story">
                         <h4>Our Pastor</h4>
-                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.</p>
+                        <p className="story-paragraph">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.
+                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora
+                         adipisicing elit Aperiam eveniet pariatur ullam tempore tempora.</p>
                     </div>
     </div>
     </div>
