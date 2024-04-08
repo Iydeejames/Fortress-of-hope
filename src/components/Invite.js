29f5e3e48f7a440195eb92da3cfda4eb76438c00
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-08 10:58:54 -0700

    Codes Added

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import backgroundImage from "../assets/imgs/FI22.JPG";
 
 const InviteSection = () => {
 
   return (
+    <section className="invite-section" style={{backgroundImage: `url(${backgroundImage})`}}>
     <div className="invite-container">
         <h2> Jesus the Same Yesterday, Today and Forever!</h2>
     </div>
+    </section>
   )
 }
 
