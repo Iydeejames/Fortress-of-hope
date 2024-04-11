f6cceb62dee044ddf3ae2d90adef5b4837ceaefd
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-11 14:38:16 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaWhatsapp, FaInstagram, FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
 
 const FooterSection = () => {
 
@@ -13,7 +13,23 @@ return(
     <a href="#online-giving">Online giving</a>
 </div>
 
-
+<div className="social-media">
+        <a href="https://wa.me/233247447640" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp />
+        </a>
+        <a href="https://www.instagram.com/raisedinitiatives?igshid=YTQwZjQ0Nml0OA==" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://www.x.com/raisedinitiative?t=VlwTiilHy96dk_RPltc4yA&s=09" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://www.facebook.com/raisedinitiatives?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+        <a href="https://www.linkedin.com/company/raisedinitiatives/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </div>
 <p className="copyright">Designed by IydeeJames 2024</p>
 {/*Add a link to the copyright*/}
 </footer>
