516c71ebf4e460acf2953a218eb1a88a7669607d
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-15 10:20:35 -0700

    Update OurPastors.js

--- src/components/OurPastors.js
+++ src/components/OurPastors.js
@@ -2,27 +2,32 @@ import React, { useState } from 'react';
 import firstPhoto from '../assets/imgs/FI5.JPG';
 import secondPhoto from '../assets/imgs/FI21.JPG';
 
-
 const PastorsSection = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenFirst, setIsOpenFirst] = useState(false);
+    const [isOpenSecond, setIsOpenSecond] = useState(false);
 
-    const toggleStory = () => {
-        setIsOpen(!isOpen);
+    const toggleStoryFirst = () => {
+        setIsOpenFirst(!isOpenFirst);
+        setIsOpenSecond(false); // Close the second card when opening the first one
+    };
+
+    const toggleStorySecond = () => {
+        setIsOpenSecond(!isOpenSecond);
+        setIsOpenFirst(false); // Close the first card when opening the second one
     };
 
     return (
         <div className='pastors-section'>
             <h2>Meet Our Pastors</h2>
             <div className='cards-container'>
-            <div className='floating-bg'></div>
                 <div className='card-content'>
-                    <img src={firstPhoto} alt='Rev Amba' />
+                    <img src={firstPhoto} alt='Rev Amba' style={{ opacity: isOpenFirst ? 0 : 1 }} />
                     <h3>Rev. Prince Amba<span> (General Overseer) </span></h3>
-                    <div className='story-btn' onClick={toggleStory}>
+                    <div className='story-btn' onClick={toggleStoryFirst}>
                         About
                         <div className='story-btn-line'></div>
                     </div>
-                    {isOpen && (
+                    {isOpenFirst && (
                         <div className='story'>
                             <p className='story-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet pariatur ullam tempore tempora.</p>
                         </div>
@@ -30,13 +35,13 @@ const PastorsSection = () => {
                 </div>
 
                 <div className='card-content'>
-                    <img src={secondPhoto} alt='Dr. Evelyn' />
+                    <img src={secondPhoto} alt='Dr. Evelyn' style={{ opacity: isOpenSecond ? 0 : 1 }} />
                     <h3>Dr. Evelyn Amba<span> (Lead Pastor) </span></h3>
-                    <div className='story-btn' onClick={toggleStory}>
+                    <div className='story-btn' onClick={toggleStorySecond}>
                         About
                         <div className='story-btn-line'></div>
                     </div>
-                    {isOpen && (
+                    {isOpenSecond && (
                         <div className='story'>
                             <p className='story-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet pariatur ullam tempore tempora.</p>
                         </div>
