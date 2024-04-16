ad0d9cc2863184f29bc49efbed7d2a625d5171c8
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-16 15:24:58 -0700

    Update routes.jsx

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,10 +2,10 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import BranchDetails from '../components/landingPage/BranchDetails';
 import AboutUs from '../components/landingPage/AboutUs';
-import WhatWeDo from '../components/landingPage/WhatWeDo';
-import MediaResources from '../components/landingPage/MediaResources';
-import OnlineGiving from '../components/landingPage/OnlineGiving';
-import ContactUs from '../components/landingPage/ContactUs';
+import WhatWeDo from '../components/WhatWeDo';
+import MediaResources from '../components/MediaResources';
+import OnlineGiving from '../components/OnlineGiving';
+import Contact from '../components/landingPage/Contact';
 
 function PageRoutes() {
     return (
@@ -16,8 +16,8 @@ function PageRoutes() {
             <Route path="/whatwedo" element={<WhatWeDo />} />
             <Route path="/mediaresources" element={<MediaResources />} />
             <Route path="/onlinegiving" element={<OnlineGiving />} />
-            <Route path="/contactus" element={<ContactUs />} />
-            
+            <Route path="/contact" element={<Contact />} />
+
         {/* <Authlayout/> */}
         {/* <Outlet/> */}
       </Routes>
