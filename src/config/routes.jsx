535914659f998318718637bd5c22a630f9dcec93
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-16 12:07:20 -0700

    Update routes.jsx

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,6 +2,7 @@
 import {Routes, Route, Outlet} from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import Authlayout from '../layouts/authlayout';
+import BranchDetails from '../components/landingPage/BranchDetails';
 
 
 
@@ -9,6 +10,7 @@ function PageRoutes() {
   return (
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/BranchDetails" element={<BranchDetails />} />
         {/* <Authlayout/> */}
         {/* <Outlet/> */}
       </Routes>
