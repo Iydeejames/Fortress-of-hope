7745caa02a40ac2b0c5090f8678e8bf5b6f34bd3
Idaraobong James <105876361+Iydeejames@users.noreply.github.com>
2024-04-17 11:59:56 -0700

    Additions

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -2,17 +2,8 @@ import React from 'react';
 
 const AboutSection = () => {
   return (
-    <div className="about-section">
-      <div className="about-card">
-        <h2 className="about-heading">About Us</h2>
-        <p className="about-text">
-          Fortress of Hope Gospel Center is a spiritual home where elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
-          Praesent ullamcorper risus a elit. Pellentesque venenatis, ligula sit amet malesuada suscipit, purus elit ultrices nulla, non semper arcu lectus sit amet nunc.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-        </p>
-      </div>
+    <div className="what-we-do">
+
     </div>
   );
 };
